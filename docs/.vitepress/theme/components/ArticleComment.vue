0d5a62f43516db741e812b4426aa4f0668e96c9b
zhengdayong <developer.vip@outlook.com>
2024-03-23 01:49:56 +0800

    添加评论

--- docs/.vitepress/theme/components/ArticleComment.vue
+++ docs/.vitepress/theme/components/ArticleComment.vue
@@ -1,24 +1,21 @@
 <script setup lang="ts">
-import Twikoo from "twikoo";
-import { watch, nextTick, ref, onMounted } from "vue";
-import { useData, useRouter } from "vitepress";
-const artalkEl = ref<HTMLElement | null>(null);
-const router = useRouter();
-const page = useData().page;
+import { nextTick, onMounted } from "vue";
 
 onMounted(async () => {
   await nextTick();
-  Twikoo({
-    envId: "https://commentdata.netlify.app/.netlify/functions/twikoo", // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
-    el: "#tcomment", // 容器元素
+  import("twikoo").then((Twikoo) => {
+    console.log(Twikoo, 'ArticleComment-7')
+    Twikoo.init({
+      envId: "https://commentdata.netlify.app/.netlify/functions/twikoo", // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+      el: "#tcomment", // 容器元素
+    });
   });
 });
-
 </script>
 
 
 <template>
-  <div id="tcomment" ref="artalkEl" style="margin-top: 20px"></div>
+  <div id="tcomment" style="margin-top: 20px"></div>
 </template>
 
 <style></style>
