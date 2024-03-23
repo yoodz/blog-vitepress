3efe682405ae951e14bd7dc6bd15b7d888bb956f
zhengdayong <developer.vip@outlook.com>
2024-03-23 23:56:42 +0800

    keywords

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,9 +3,26 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Afunny",
-  description: "A VitePress Site",
+  description: "afunny.top",
   appearance: false,
-
+  head: [
+    [
+      "meta",
+      {
+        name: "keywords",
+        content:
+          "科技,旅行,生活方式,程序员,互联网,自媒体,摄影师,编程,前端,前端工程师,独立博客,Vlog",
+      },
+    ],
+    [
+      "meta",
+      {
+        name: "description",
+        content:
+          "",
+      },
+    ],
+  ],
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
