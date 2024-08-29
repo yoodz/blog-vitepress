40ac793c9c132ac2fafd77210b85504bcd7ea6a6
yoodz <904201050@qq.com>
2024-08-29 19:06:49 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -74,107 +74,99 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/yoodz' }
     ],
     search: {
-      provider: 'local',
-      options: {
-        _render(src, env, md) {
-          const html = md.render(src, env)
-          if (env.frontmatter?.title)
-            return md.render(`# ${env.frontmatter.title}`) + html
-          return html
-        },
-        locales: {
-          root: {
-            translations: {
-              button: {
-                buttonText: '搜索文档',
-                buttonAriaLabel: '搜索文档'
-              },
-              modal: {
-                noResultsText: '无法找到相关结果',
-                displayDetails: '显示详细信息',
-                resetButtonTitle: '清除查询条件',
-                backButtonTitle: '返回搜索结果',
-                footer: {
-                  selectText: '选择',
-                  selectKeyAriaLabel: 'enter',
-                  navigateText: '切换',
-                  navigateUpKeyAriaLabel: 'up arrow',
-                  navigateDownKeyAriaLabel: 'down arrow',
-                  closeText: '关闭',
-                  closeKeyAriaLabel: 'escape'
-                }
-              }
-            }
-          }
-        },
-
-        miniSearch: {
-          searchOptions: {
-
-          }
-        },
-      },
+      // provider: 'local',
       // options: {
-      //   appId: '6YIA30YW3V',
-      //   apiKey: 'b5475f913352c0ff58d72d07c0178cbc',
-      //   indexName: 'afunny',
+      //   _render(src, env, md) {
+      //     const html = md.render(src, env)
+      //     if (env.frontmatter?.title)
+      //       return md.render(`# ${env.frontmatter.title}`) + html
+      //     return html
+      //   },
       //   locales: {
-      //     zh: {
-      //       placeholder: '搜索文档',
+      //     root: {
       //       translations: {
       //         button: {
       //           buttonText: '搜索文档',
       //           buttonAriaLabel: '搜索文档'
       //         },
       //         modal: {
-      //           searchBox: {
-      //             resetButtonTitle: '清除查询条件',
-      //             resetButtonAriaLabel: '清除查询条件',
-      //             cancelButtonText: '取消',
-      //             cancelButtonAriaLabel: '取消'
-      //           },
-      //           startScreen: {
-      //             recentSearchesTitle: '搜索历史',
-      //             noRecentSearchesText: '没有搜索历史',
-      //             saveRecentSearchButtonTitle: '保存至搜索历史',
-      //             removeRecentSearchButtonTitle: '从搜索历史中移除',
-      //             favoriteSearchesTitle: '收藏',
-      //             removeFavoriteSearchButtonTitle: '从收藏中移除'
-      //           },
-      //           errorScreen: {
-      //             titleText: '无法获取结果',
-      //             helpText: '你可能需要检查你的网络连接'
-      //           },
+      //           noResultsText: '无法找到相关结果',
+      //           displayDetails: '显示详细信息',
+      //           resetButtonTitle: '清除查询条件',
+      //           backButtonTitle: '返回搜索结果',
       //           footer: {
       //             selectText: '选择',
+      //             selectKeyAriaLabel: 'enter',
       //             navigateText: '切换',
+      //             navigateUpKeyAriaLabel: 'up arrow',
+      //             navigateDownKeyAriaLabel: 'down arrow',
       //             closeText: '关闭',
-      //             searchByText: '搜索提供者'
-      //           },
-      //           noResultsScreen: {
-      //             noResultsText: '无法找到相关结果',
-      //             suggestedQueryText: '你可以尝试查询',
-      //             reportMissingResultsText: '你认为该查询应该有结果？',
-      //             reportMissingResultsLinkText: '点击反馈'
+      //             closeKeyAriaLabel: 'escape'
       //           }
       //         }
       //       }
       //     }
-      //   }
-      // }
+      //   },
+
+      //   miniSearch: {
+      //     searchOptions: {
+
+      //     }
+      //   },
+      // },
+      provider: 'algolia',
+      options: {
+        appId: '6YIA30YW3V',
+        apiKey: 'fbb922f7f1231c79375dac8fea03af6c',
+        indexName: 'afunny',
+        locales: {
+          zh: {
+            placeholder: '搜索文档',
+            translations: {
+              button: {
+                buttonText: '搜索文档',
+                buttonAriaLabel: '搜索文档'
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: '清除查询条件',
+                  resetButtonAriaLabel: '清除查询条件',
+                  cancelButtonText: '取消',
+                  cancelButtonAriaLabel: '取消'
+                },
+                startScreen: {
+                  recentSearchesTitle: '搜索历史',
+                  noRecentSearchesText: '没有搜索历史',
+                  saveRecentSearchButtonTitle: '保存至搜索历史',
+                  removeRecentSearchButtonTitle: '从搜索历史中移除',
+                  favoriteSearchesTitle: '收藏',
+                  removeFavoriteSearchButtonTitle: '从收藏中移除'
+                },
+                errorScreen: {
+                  titleText: '无法获取结果',
+                  helpText: '你可能需要检查你的网络连接'
+                },
+                footer: {
+                  selectText: '选择',
+                  navigateText: '切换',
+                  closeText: '关闭',
+                  searchByText: '搜索提供者'
+                },
+                noResultsScreen: {
+                  noResultsText: '无法找到相关结果',
+                  suggestedQueryText: '你可以尝试查询',
+                  reportMissingResultsText: '你认为该查询应该有结果？',
+                  reportMissingResultsLinkText: '点击反馈'
+                }
+              }
+            }
+          }
+        }
+      }
     }
   },
   cleanUrls: true,
   lang: "zh-CN",
-  docFooter: {
-    prev: '上一篇',
-    next: '下一篇',
-  },
-  returnToTopLabel: '回到顶部',
-  sidebarMenuLabel: '菜单',
-  darkModeSwitchLabel: '主题',
-  lightModeSwitchTitle: '切换到浅色模式',
-  darkModeSwitchTitle: '切换到深色模式',
   sitemap: {
     hostname: 'https://www.afunny.top'
   }
