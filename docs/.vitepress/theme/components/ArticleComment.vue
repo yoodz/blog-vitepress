4ebbd2dc34f7348f95d3f1e403f5e6bb949f2e7a
yoodz <904201050@qq.com>
2025-02-03 22:15:27 +0800

    更新评论功能

--- docs/.vitepress/theme/components/ArticleComment.vue
+++ docs/.vitepress/theme/components/ArticleComment.vue
@@ -1,24 +1,21 @@
-<!-- 详情页的评论模块 -->
 <script setup lang="ts">
 import { nextTick, onMounted } from "vue";
 
 onMounted(async () => {
   await nextTick();
-  // import("artalk").then((Artalk) => {
-  //   Artalk.init({
-  //     el: '#Comments',                // 绑定元素的 Selector
-  //     // pageKey:   '/post/1',                  // 固定链接
-  //     // pageTitle: '关于引入 Artalk 的这档子事',  // 页面标题 (留空自动获取)
-  //     server: 'https://c.afunny.top:4446',  // 后端地址
-  //     site: 'Afunny 的博客',             // 你的站点名
-  //   })
-  // });
+  import("twikoo").then((Twikoo) => {
+    console.log(Twikoo, 'ArticleComment-7')
+    Twikoo.init({
+      envId: "https://commentdata.netlify.app/.netlify/functions/twikoo", // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
+      el: "#tcomment", // 容器元素
+    });
+  });
 });
 </script>
 
 
 <template>
-  <div id="Comments"></div>
+  <div id="tcomment" style="margin-top: 20px"></div>
 </template>
 
 <style></style>
