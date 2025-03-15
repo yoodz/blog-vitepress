989a32658b63ade759c6b9a66fac703f5181aae3
yoodz <904201050@qq.com>
2025-03-15 18:20:16 +0800

    1

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -237,6 +237,6 @@ export default defineConfig({
       })
     }
   
-    writeFileSync(path.join(config.outDir, 'feed.rss'), feed.rss2(), { encoding: 'utf8' })
+    writeFileSync(path.join(config.outDir, 'feed.rss'), feed.rss2(), { encoding: 'utf-8' })
   }
 })
